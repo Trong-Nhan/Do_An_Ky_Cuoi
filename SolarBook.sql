af0dbd2bfaa1e4398db508eed17e40076d98da07
Trong-Nhan <nhanbui128@gmail.com>
2022-06-27 23:43:57 +0700

    test_2
    bổ sung file báo cáo, file slide, file cơ sở dữ liệu của đồ án, tất cả đều là bài mẫu, vẫn chưa sửa chữa lại cho phù hợp với nội dung của đề tài

---
+++ SolarBook.sql
@@ -0,0 +1,131 @@
+Create Database SolarBook
+GO
+Use SolarBook
+GO
+
+Create Table tblCategory(
+	Id int primary key identity,
+	[Name] nvarchar(100) unique
+)
+GO
+
+Create Table tblPublisher(
+	Id int primary key identity,
+	[Name] nvarchar(100)
+)
+
+GO
+
+Create Table tblBook(
+	Id int primary key identity,
+	[Name] nvarchar(100),
+	CategoryId int,
+	Price float,
+	SalePrice float Default(0),
+	Author nvarchar(100),
+	PublisherId int,
+	PublishYear int,
+	Picture nvarchar(255),
+	Number int,
+	[Description] text, 
+	[Page] int, 
+	Rating float,
+	[Status] bit,
+
+	Foreign Key (CategoryId) References tblCategory(Id),
+	Foreign Key (PublisherId) References tblPublisher(Id)
+)
+GO
+
+Create Table tblCity(
+	Id int primary key identity,
+	[Name] nvarchar(50)
+)
+GO
+
+Create Table tblUser(
+	Id int primary key identity, 
+	[Name] nvarchar(150),
+	Phone varchar(10),
+	[Password] varchar(50),
+	Email varchar(100),
+	CityId int,
+	[Address] text,
+	role varchar(10),
+
+	Foreign Key (CityId) References tblCity(Id)
+)
+GO
+
+Create Table tblPayment(
+	Id int primary key identity,
+	[Name] nvarchar(150)
+)
+GO
+
+Create Table tblPromoCode(
+	Id int primary key identity,
+	[Name] varchar(20),
+	[Description] nvarchar(255),
+	[Type] bit,
+	[Value] int
+)
+GO
+
+Create Table tblOrder(
+	Id int primary key identity,
+	UserId int,
+	OrderDate datetime,
+	PromoCodeId int,
+	TotalPrice float,
+	CityId int,
+	ShippingAddress text,
+	ShippingPrice int,
+	PaymentId int,
+	Note nvarchar(255),
+	CreatedDate Date
+
+	Foreign Key (UserId) References tblUser(Id),
+	Foreign Key (PromoCodeId) References tblPromoCode(Id),
+	Foreign Key (CityId) References tblCity(Id),
+	Foreign Key (PaymentId) References tblPayment(Id)
+)
+GO
+
+Create Table tblOrderDetail(
+	OrderId int,
+	BookId int,
+	Number int,
+
+	Primary Key (OrderId, BookId),
+	Foreign Key (OrderId) References tblOrder(Id),
+	Foreign Key (BookId) References tblBook(Id)
+)
+GO
+
+Create Table tblNews(
+	Id int primary key identity,
+	[Name] nvarchar(255),
+	[Description] nvarchar(255),
+	Detail text,
+	[Picture] nvarchar(255),
+	CreatedDate Date
+)
+
+GO
+
+Create Table tblRating(
+	Id int primary key identity,
+	UserId int,
+	BookId int, 
+	Rating int,
+	Comment text,
+	CreatedDate Date
+
+	Foreign Key (UserId) References tblUser(Id),
+	Foreign Key (BookId) References tblBook(Id)
+)
+
+GO
+
+Select * From tblBook
