eb54ec6a6c835ccc7abb944f555bf4cbb8e8122e
Trong-Nhan <nhanbui128@gmail.com>
2022-12-23 11:04:16 +0700

    commit
    sửa lỗi khi ấn nút quay lại ở màn hình đăng nhập, màn hình quản trị và màn hình trang chủ

--- ProjectFinal/app/src/main/java/com/example/projectfinal/activity/LoginActivity.java
+++ ProjectFinal/app/src/main/java/com/example/projectfinal/activity/LoginActivity.java
@@ -208,4 +208,12 @@ public class LoginActivity extends AppCompatActivity {
             startActivity(intent);
         }
     };
+
+    @Override
+    public void onBackPressed() {
+        Intent intent = new Intent(Intent.ACTION_MAIN);
+        intent.addCategory(Intent.CATEGORY_HOME);
+        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        startActivity(intent);
+    }
 }
